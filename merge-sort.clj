df7feb4ffe4507904ce9542b1f9d8435c3b18e4e
Thomas Varney <thomasvarney723@gmail.com>
2016-11-16 17:46:35 -0600

    cleanup

--- merge-sort.clj
+++ merge-sort.clj
@@ -3,21 +3,20 @@
   ([coll1 coll2]
    (cond (empty? coll1) coll2
          (empty? coll2) coll1
-         :else 
-         (lazy-seq
-          (let [[f1 & r1 :as c1] (seq coll1)
-                [f2 & r2 :as c2] (seq coll2)]
-            (if (<= f1 f2) 
-              (cons f1 (merge-back r1 c2))
-              (cons f2 (merge-back r2 c1))))))))
+         :else (lazy-seq
+                (let [[f1 & r1 :as c1] (seq coll1)
+                      [f2 & r2 :as c2] (seq coll2)]
+                  (if (<= f1 f2) 
+                    (cons f1 (merge-back r1 c2))
+                    (cons f2 (merge-back r2 c1))))))))
                
 (defn merge-sort [coll]
-  (let [part #(partition-all 2 %)
-        map-apply-merge (partial map #(apply merge-back %))]
+  (letfn [(part [s]
+             (partition-all 2 s))
+          (map-apply-merge [s]
+             (map #(apply merge-back %) s))]
     (->> (reductions #(%2 %1)
                     (map vector coll)
-                    (cycle [part map-apply-merge]))
+                    (repeat (comp map-apply-merge part)))
          (drop-while #(> (count %) 1))
-         first
-         map-apply-merge
-         first)))
+         ffirst)))
