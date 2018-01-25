5d53b6ba40c17f0d61b3f6fa9c917fb9ddf17076
Thomas Varney <thomasvarney723@gmail.com>
2018-01-25 11:42:17 -0600

    new implimentation

--- merge-sort.clj
+++ merge-sort.clj
@@ -1,17 +1,30 @@
-(defn merge-seqs
-  ([coll] coll)
-  ([[h1 & t1 :as c1] [h2 & t2 :as c2]]
-   (cond (not h1) c2
-         (not h2) c1
-         :else (lazy-seq
-                  (if (<= h1 h2) 
-                    (cons h1 (merge-seqs t1 c2))
-                    (cons h2 (merge-seqs t2 c1)))))))
+(defn rpart [[x & xs]]
+    (reduce (fn [result input]
+              (if (<= (peek (peek result)) input)
+                (conj (pop result) (conj (peek result) input))
+                (conj result [input])))
+            [[x]]
+            xs))
 
-(defn merge-back [coll]
-  (map #(apply merge-seqs %) (partition-all 2 coll)))
-                              
+(defn sorted-runs [coll]
+  (let [previous (atom Integer/MIN_VALUE)]
+    (partition-by
+     (fn [n]
+       (if (<= @previous n)
+         (do (reset! previous n) :no-split)
+         (do (reset! previous n) :split)))
+     coll)))
+
+(defn merge-back
+  ([coll] coll)
+  ([[f1 & r1 :as c1] [f2 & r2 :as c2]]
+   (lazy-seq
+    (cond (not f1) c2
+          (not f2) c1
+          (<  f1 f2)  (cons f1 (merge-back c2 r1))
+          (>= f1 f2)  (cons f2 (merge-back c1 r2)))))
+  ([c1 c2 & more]
+   (reduce merge-back [] more)))
+
 (defn merge-sort [coll]
-  (->> (iterate merge-back (map vector coll))
-       (drop-while #(> (count %) 1))
-       ffirst))
+    (apply merge-back (sorted-runs coll)))
